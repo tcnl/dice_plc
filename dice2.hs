2cc5314177c18e30f815242fd6f9845ac66bcec1
Cinthya Lins <cml2@cin.ufpe.br>
2018-12-08 19:25:30 -0300

    calculo dos valores parciais

--- dice2.hs
+++ dice2.hs
@@ -4,6 +4,7 @@ import Text.Printf
 import Control.Monad
 import System.IO
 
+
 --CHECKLIST / O QUE PRECISA (ORDEM DE PRIORIDADE)
 -- 1- Fazer algo similar a um while pro jogo poder ficar rodando, dei uma lida e talvez algo semelhante a recursao
 -- 2- Arrumar os if/else, ja que eles que vao ditar a pontuacao, se e positiva ou negativa
@@ -32,7 +33,7 @@ main = do
     print "Jogador 1 aposte: "
     palpite <- getLine
     let input = (read palpite :: Int)
-    
+
     --Lanca o(s) dado(s)
     --Aqui entra um if/else das dificuldades
         --Se tiver >70 pontos, lanca dois dados
@@ -43,57 +44,105 @@ main = do
     --Inicia duas thread, uma salva a aposta na MVar e bloqueia
     --A outra vai ficar esperando a MVar ficar livra pra colocar o resultado do(s) dado(s)
     forkIO $ do putMVar jogo input; putMVar jogo number
-    
+
     --Pega o valor de MVar(aposta inicial) e coloca e salva
     aposta <- takeMVar jogo
     --Assim a MVar fica livre e recebe o valor dos dados (acima)
-    print aposta
+    print $ "Aposta: " ++ show aposta
 
     --MVar fica livre de novo
     dado   <- takeMVar jogo
-    print dado
+    print $ "Dado: " ++ show dado
+
+    let parcial1 = 50
 
     --Se a pessoa acertar o valor do dado, ganha os pontos se nao, perde.
     --Mas o if/else ta sempre dando erro de parse
-    --if(aposta == dado) then 
+    --if(aposta == dado) then
     --     let parcial1 =  aposta
     --else let parcial1 = -aposta
+
+    -- parcial1 :: Int -> Int -> Int
+    -- parcial1 aposta dado =
+    --     let acerto = (parcial1 = parcial1 + aposta)
+    --         erro = (parcial1 = parcial1 - aposta)
+    --     if (aposta = dado)
+    --         then acerto
+    --         else erro
+
+    -- parcial1 aposta dado
+    --     | aposta == dado = acerto
+    --     | otherwise = erro
+    --     where   acerto = (parcial1 = parcial1 + dado)
+    --             erro = (parcial1 = parcial1 - dado)
+
+    -- parcial_um :: Int -> Int -> Int -> Int
+    -- parcial_um aposta dado parcial1
+    --     | aposta == dado = acerto
+    --     | otherwise = erro
+    --     where   acerto = (let parcial1 = (parcial1 + aposta))
+    --             erro = (let parcial1 = (parcial1 - aposta))
+
+    -- parcial_um :: Int -> Int -> Int -> Int
+    -- parcial_um aposta dado parcial1 =
+    --     if aposta == dado then (parcial1 + aposta)
+    --     else (parcial1 - aposta)
+
+        parcial_um :: Int -> Int -> Int -> Int
+        parcial_um aposta dado parcial1
+            | aposta == dado = (parcial1 + aposta)
+            | otherwise = (parcial1 - aposta)
+
+ 
+    print $ "Parcial 1: " ++ show (parcial_um aposta dado parcial1)
+
+
 
     --O mesmo de cima, so que pro Jogador 2
     print "Jogador 2 aposte: "
     palpite <- getLine
     let input = (read palpite :: Int)
-    
+
     --Lanca o(s) dado(s)
     number <- randomRIO (1,6) :: IO Int
 
     --Inicia duas thread, uma salva a aposta na MVar e bloqueia
     --A outra vai ficar esperando a MVar ficar livra pra colocar o resultado do(s) dado(s)
     forkIO $ do putMVar jogo input; putMVar jogo number
-    
+
     --Pega o valor de MVar(aposta inicial) e coloca e salva
     aposta <- takeMVar jogo
     --Assim a MVar fica livre e recebe o valor dos dados (acima)
-    print aposta
+    print $ "Aposta: " ++ show aposta
 
     --MVar fica livre de novo
     dado   <- takeMVar jogo
-    print dado
-    --if(aposta == dado) then 
+    print $ "Dado: " ++ show dado
+    --if(aposta == dado) then
     --     let parcial2 =  aposta
     --else let parcial2 = -aposta
-
-    forkIO $ do putMVar tabela parcial1; putMVar tabela number
-    --Salva a tabela original [50,50]
-    auxTabela <- takeMVar tabela
-    --tabela livre, MVar recebe parcial1
-
-    let auxTabela[0] = auxTabela[0] + (takeMVar tabela)
-    --valor dos pontos do J1 ajustados, libera tabela
-
-    let auxTabela[1] = auxTabela[1] + (takeMVar tabela)
-    --valor dos pontos do J2 ajustados, libera tabela
-    putMVar tabela auxTabela
     
-    --Fim do while
-    
+    let parcial2 = 50
+
+        parcial_dois :: Int -> Int -> Int -> Int
+        parcial_dois aposta dado parcial2
+            | aposta == dado = (parcial2 + aposta)
+            | otherwise = (parcial1 - aposta)
+
+
+    print $ "Parcial 2: " ++ show (parcial_dois aposta dado parcial2)
+
+    -- forkIO $ do putMVar tabela parcial1; putMVar tabela number
+    -- --Salva a tabela original [50,50]
+    -- auxTabela <- takeMVar tabela
+    -- --tabela livre, MVar recebe parcial1
+
+    -- let auxTabela[0] = auxTabela[0] + (takeMVar tabela)
+    -- --valor dos pontos do J1 ajustados, libera tabela
+
+    -- let auxTabela[1] = auxTabela[1] + (takeMVar tabela)
+    -- --valor dos pontos do J2 ajustados, libera tabela
+    -- putMVar tabela auxTabela
+
+    -- --Fim do while
+
